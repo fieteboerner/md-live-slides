0a54c29976e3ff33b0c70bf83f5815688190a222
Fiete Börner <fieteboerner@googlemail.com>
2019-04-20 00:03:31 +0200

    fix FE route

--- client/src/router.js
+++ client/src/router.js
@@ -14,7 +14,7 @@ export default new Router({
       component: Home,
     },
     {
-      path: 'presentations',
+      path: '/presentations',
       name: 'presentations',
       component: PresentationList,
     },
