317face1a11f4843bf27741409a05e4d9cc46505
Fiete Börner <fieteboerner@googlemail.com>
2019-04-21 02:14:57 +0200

    move layout to a separate component

--- client/src/router.js
+++ client/src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Page from './views/Page.vue';
 import Home from './views/Home.vue';
 import PresentationList from './views/PresentationList.vue';
 import PresentationEdit from './views/PresentationEdit.vue';
@@ -7,22 +8,28 @@ import PresentationEdit from './views/PresentationEdit.vue';
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/presentations',
-      name: 'presentations',
-      component: PresentationList,
-    },
-    {
-      path: '/presentation/:id',
-      name: 'presentation.edit',
-      component: PresentationEdit,
-    },
-  ],
+    mode: 'history',
+    routes: [
+        {
+            path: '',
+            component: Page,
+            children: [
+                {
+                    path: '',
+                    name: 'home',
+                    component: Home,
+                },
+                {
+                    path: '/presentations',
+                    name: 'presentations',
+                    component: PresentationList,
+                },
+                {
+                    path: '/presentation/:id',
+                    name: 'presentation.edit',
+                    component: PresentationEdit,
+                },
+            ],
+        },
+    ],
 });
---
+++ client/src/views/Page.vue
@@ -0,0 +1,110 @@
+<template>
+    <div id="app">
+        <nav
+            class="navbar"
+            role="navigation"
+            aria-label="main navigation"
+        >
+            <div
+                v-click-outside="onHideMenu"
+                class="navbar-brand"
+            >
+                <router-link
+                    :to="{ name: 'home' }"
+                    class="navbar-item"
+                >
+                    Home
+                </router-link>
+
+                <a
+                    role="button"
+                    class="navbar-burger burger"
+                    aria-label="menu"
+                    aria-expanded="false"
+                    data-target="navbarBasicExample"
+                    @click="menuOpen = !menuOpen"
+                >
+                    <span aria-hidden="true" />
+                    <span aria-hidden="true" />
+                    <span aria-hidden="true" />
+                </a>
+            </div>
+
+            <div
+                id="navbarBasicExample"
+                class="navbar-menu"
+                :class="{ 'is-active': menuOpen }"
+            >
+                <div class="navbar-start">
+                    <router-link
+                        :to="{ name: 'presentations' }"
+                        class="navbar-item"
+                    >
+                        Presentations
+                    </router-link>
+                </div>
+
+                <div class="navbar-end">
+                    <a
+                        class="navbar-item"
+                        @click.prevent="onCreatePresentation"
+                    >
+                        + Presentation
+                    </a>
+                </div>
+            </div>
+        </nav>
+        <div class="content">
+            <router-view />
+        </div>
+    </div>
+</template>
+
+<script>
+import ClickOutside from 'v-click-outside'
+import PresentationService from '@/services/Presentation';
+
+export default {
+    name: 'App',
+    directives: {
+        ClickOutside: ClickOutside.directive,
+    },
+    data() {
+        return {
+            menuOpen: false,
+        };
+    },
+    methods: {
+        onHideMenu() {
+            this.menuOpen = false;
+        },
+        async onCreatePresentation() {
+            const presentation = await PresentationService.create();
+
+            this.$router.push({
+                name: 'presentation.edit',
+                params: { id: presentation.key },
+            });
+        }
+    },
+}
+</script>
+
+
+<style lang="scss">
+@import "../assets/scss/bulma/index.scss";
+
+html, body {
+    height: 100%;
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+
+    .content {
+        flex-grow: 1;
+    }
+}
+</style>
